119de21cae840781952d260a9da7b88a9526d24a
Tom Taylor <hello@tomtaylor.name>
2017-02-18 23:55:47 +0000

    Start to set up as standalone package

--- install.js
+++ install.js
@@ -1,7 +1,11 @@
+var _ = require('underscore');
+var async = require('async');
+var exec = require('child_process').exec;
 var finalhandler = require('finalhandler')
 var http = require("http");
 var open = require("open");
 var serveStatic = require('serve-static');
+
 
 var PORT = 5678;
 var step = -1;
@@ -14,12 +18,9 @@ var server = http.createServer(function onRequest(req, res) {
       serve(req, res, finalhandler);
       break;
     case 'POST':
-      doNextStep(function(label, text) {
+      doNextStep(function(step) {
         // we're not expecting data, so nothing to process
-        var data = JSON.stringify({
-          buttonLabel: label,
-          text: text
-        });
+        var data = JSON.stringify(_.omit(step, 'function'));
         res.writeHead(200, { 'Content-Length': data.length });
         res.end(data);
       });
@@ -32,22 +33,25 @@ open("http://localhost:" + PORT + '/installer.html');
 function doNextStep(cb) {
   var nextStep = steps[++step];
   nextStep.function.call(this, function() {
-    cb(nextStep.label, nextStep.text);
+    cb(nextStep);
   });
 }
 
 var steps = [
   {
     label: 'Continue',
-    text: 'Something else is going to happen now...',
-    function: test
+    title: 'Prerequisites',
+    text: 'Checking all dependencies are installed.',
+    function: checkDeps
   },
   {
-    label: 'Continue 2',
+    label: 'Continue',
+    title: 'Step 2',
     text: 'Another step.',
     function: test
   },
   {
+    title: 'Install complete',
     text: 'Install has completed successfully! You can now close the page.',
     function: test
   }
@@ -55,4 +59,25 @@ var steps = [
 
 function test(cb) {
   cb();
+}
+
+function checkDeps(cb) {
+  var data = [];
+  async.eachSeries([
+    'git --version',
+    'node --version',
+    'mongo --version',
+    'ffmpeg -version'
+  ], function(task, doneEach) {
+    exec(task, function(error, stdout, stderr) {
+      if(error) return doneEach(error)
+      if(stderr) return doneEach(stderr);
+      var version = stdout.trim().match(/\d\d?.\d\d?.\d\d?/)[0];
+      data.push(version);
+      doneEach();
+    });
+  }, function() {
+    console.log('done all', data);
+    cb();
+  });
 }
